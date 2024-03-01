1ee1d54081550c89b798c061e14a0e91bd222f3f
wyatt-holliday <hollidaw@oregonstate.edu>
2024-02-29 21:15:56 -0800

    Step 3 done

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 // app.js
 
-const { engine } = require('express-handlebars');
-var exphbs = require('express-handlebars');     // Import express-handlebars
-app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+/*
+    SETUP
+*/
+const exphbs = require('express-handlebars');
+const express = require('express');     // Import express-handlebars
+const { engine } = exphbs;
+var app     = express();            // We need to instantiate an express object to interact with the server in our code
+PORT        = 3331;                 // Set a port number at the top so it's easy to change in the future
+
+app.engine('.hbs', exphbs.engine({ extname: ".hbs" }));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
 // Database
 var db = require('./database/db-connector')
-
-/*
-    SETUP
-*/
-var express = require('express');   // We are using the express library for the web server
-var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 3331;                 // Set a port number at the top so it's easy to change in the future
 
 /*
     ROUTES
